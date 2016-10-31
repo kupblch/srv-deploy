c62a8db6afec24f30b376ac9a2eaf2f8720cc135
kupblch <kupblch@gmail.com>
2016-10-31 18:47:52 +0400

    Update getId.py

--- hh/getId.py
+++ hh/getId.py
@@ -1,19 +1,27 @@
+import time
 import json
 from urllib import urlopen
+from datetime import datetime
 
 
 def getVacancies(query, page=0):
+    t0 = time.clock()
     response = urlopen("https://api.hh.ru/vacancies/"+query+'&page='+str(page)).read()
     responseJson = json.loads(response)
+    if (time.clock() - t0) < 1:
+        time.sleep(1 - (time.clock() - t0))
 
     for vacancy in responseJson['items']:
-        print(vacancy.get('id', ''))
+        ids.append(vacancy.get('id', '').encode('utf-8'))
 
     if responseJson['pages'] > page:
         page = page + 1
-        getVacancies('?text=%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%B0%D1%8F+%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D1%8C&enable_snippets=false&area=2', page)
+        getVacancies(query, page)
     else:
-        return(responseJson['pages'])
+        return(ids)
 
-
+ids=[]
 getVacancies('?text=%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%B0%D1%8F+%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D1%8C&enable_snippets=false&area=2')
+
+print(len(ids))
+print(ids)
